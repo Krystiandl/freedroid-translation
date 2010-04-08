490ce07eea4169487fccb680cd95593fa47b22fa
ffbb2010 <Ooh2iej7Ae@gmail.com>
2010-04-08 20:31:43 +0200

    Merge the russian translation

--- up.sh
+++ up.sh
@@ -7,5 +7,10 @@ git svn rebase ;
 cd ../dialogs ;
 git svn rebase ; 
 cd ..
+cd dialogs ;
+for i in *.dialog ; do  # update the pot files
+    ../extract-strings-from-data.py . "${i}" "../po/${i/.dialog/.pot}" ;
+done ;
+
 git commit -a
 git push
