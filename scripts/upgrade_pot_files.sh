40c9ed96f2878c06c6f3199c5acccd1013cf92b8
ffbb2010 <Ooh2iej7Ae@gmail.com>
2010-04-09 14:24:22 +0200

    modifying the upgrade_pot_files.sh to take in account the location in the script repository

--- scripts/upgrade_pot_files.sh
+++ scripts/upgrade_pot_files.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd .. ;
+
 cd map ;
 git svn rebase ;
 cd ../src ;
@@ -11,14 +13,14 @@ git svn rebase ;
 cd ..
 cd dialogs ;
 for i in *.dialog ; do  # update the pot files
-    ../extract-strings-from-data.py . "${i}" "../po/${i/.dialog/.pot}" ;
+    ../scripts/extract-strings-from-data.py . "${i}" "../po/${i/.dialog/.pot}" ;
 done ;
 
 # update the pot files for any msg strings in the map directory
 cd .. ;
 cd map ;
 for i in $(grep -l '_"' *) ; do # update the pot files for items and title files
-    ../extract-strings-from-data.py . "${i}" "../po/${i}.pot" ;
+    ../scripts/extract-strings-from-data.py . "${i}" "../po/${i}.pot" ;
 done ;
 
 cd .. ;
