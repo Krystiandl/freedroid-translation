5fe5c5e0711c5cb740ee3def919587222c5e9b66
ffbb2010 <Ooh2iej7Ae@gmail.com>
2010-04-21 13:47:53 +0200

    adapt the script merge_current_translation.sh for fitting the needs

--- scripts/merge_current_translation.sh
+++ scripts/merge_current_translation.sh
@@ -5,32 +5,58 @@
 # the initial translation contains four language translation
 # with an english source
 
+# extract the translation
+
+
+cd ..
+cd dialogs ;
+echo "in $(pwd)" ;
+
+for i in *.dialog ; do
+    ../scripts/extract-strings-from-data.py "$(pwd)" "${i}" ../po/${i/.dialog/.pot} ;
+done ;
+
+cd .. ;
+cd map ;  
+echo "in $(pwd)" ;
+
+for i in $(grep -l '_"' *) ; do
+    ../scripts/extract-strings-from-data.py "$(pwd)" "${i}" ../po/${i}.pot
+done ;
+
+git commit -a -m "update of the pot files from the dialogs/ and map/ directory"
+
+# merge the translation with the new pots files
 
 for lang in "fr_FR" "sv_SE" "ru_RU" "de_DE" ; do
-     
-    cd .. ;
+
     echo "For the language ${lang}"
-    # the initial .po file containing the initial dialog translation
-    # is called freedroidrpg.po
+    cd .. ;
+
+    # updating the .po files for the dialogs (dialogs/ directory)
     cd dialogs ;
     echo "in $(pwd)" ;
     
     for i in *.dialog ; do
-        ../scripts/extract-strings-from-data.py "$(pwd)" "${i}" ../po/${i/.dialog/.pot} ;
-        msgmerge ../po/${lang}/LC_MESSAGES/freedroidrpg_dialogs.po ../po/${i/.dialog/.pot} > ../po/${lang}/LC_MESSAGES/${i/.dialog/.po} ;
+        msgmerge  ../po/${lang}/LC_MESSAGES/${i/.dialog/.po}  ../po/${i/.dialog/.pot} > ../po/${lang}/LC_MESSAGES/${i/.dialog/.po}.tmp ;
+        mv -v ../po/${lang}/LC_MESSAGES/${i/.dialog/.po}.tmp ../po/${lang}/LC_MESSAGES/${i/.dialog/.po}
     done ;
 
+    git commit -a -m "Merge the translation with new pot files from the dialogs/directory and lang ${lang}" ;
     # item part
+    
+
     cd .. ;
 
-    # the initial .po file containing the initial translation is 
-    # called freedroidrpg_data.po
+    # updating the .po files for the items (map/ directory)
     cd map ;
     echo "in $(pwd)" ;
 
     for i in $(grep -l '_"' *) ; do
-        ../scripts/extract-strings-from-data.py "$(pwd)" "${i}" ../po/${i}.pot
-        msgmerge  ../po/${lang}/LC_MESSAGES/freedroidrpg_data.po ../po/${i}.pot > ../po/${lang}/LC_MESSAGES/${i}.po ;
+        msgmerge  ../po/${lang}/LC_MESSAGES/${i}.po ../po/${i}.pot > ../po/${lang}/LC_MESSAGES/${i}.po.tmp ;
+        mv -v ../po/${lang}/LC_MESSAGES/${i}.po.tmp ../po/${lang}/LC_MESSAGES/${i}.po
     done ;
+
+    git commit -a -m "Merge the translation with new pot files from the map/ directory and lang ${lang}" ;
 
 done ;    
