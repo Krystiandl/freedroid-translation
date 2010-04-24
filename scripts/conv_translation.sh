b448a0d6afa07af6de043971de15f95c5b18b79f
ffbb2010 <Ooh2iej7Ae@gmail.com>
2010-04-24 17:33:48 +0200

    Massive conversion to UTF8

--- scripts/conv_translation.sh
+++ scripts/conv_translation.sh
@@ -2,13 +2,12 @@
 # this script convert all the po files of the translation 
 # from the encoding X to the encoding given as first argument
 
-
 cd ..
 cd po
 
-for lang in $(cat lang.txt) ; do # all the po files
+for lang in $(cat ../scripts/lang.txt) ; do # all the po files
     for po_file in ${lang}/LC_MESSAGES/*.po ; do # all the po files
-        msgconv  -o ${po_file}.tmp -t UTF8 -v -w 72 ${po_file} 
+        msgconv  -o ${po_file}.tmp -t UTF-8 -w 72 ${po_file} 
         mv -v ${po_file}.tmp ${po_file}
     done ;
 done ;
