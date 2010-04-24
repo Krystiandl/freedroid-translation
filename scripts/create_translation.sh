68385d5f61a5c41cfeb80e71067ef55f538285ae
ffbb2010 <Ooh2iej7Ae@gmail.com>
2010-04-24 16:34:12 +0200

    Update the script for the addition of greek, hindi

--- scripts/create_translation.sh
+++ scripts/create_translation.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+cd ..
+cd po/
+
+echo "for the language code ${1}"
+pwd
+if [ ! -d "${1}/LC_MESSAGES" ]; then 
+    mkdir "${1}" "${i}/LC_MESSAGES" ;
+fi ;
+
+for i in *.pot ; do  # on all the pot files 
+
+    msginit -i "${i}" -o "${1}/LC_MESSAGES/${i/.pot/.po}" -l ${1} -w 72 --no-translator ;
+
+done ;
