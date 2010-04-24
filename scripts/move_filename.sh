4e8f3de34fd2d8e0b40f631bcf1c083cdca7093e
ffbb2010 <Ooh2iej7Ae@gmail.com>
2010-04-24 16:09:49 +0200

    A new script to change the name of a dialog file in the translation. Used on the revision 2637-2641 of the main svn branch

--- scripts/move_filename.sh
+++ scripts/move_filename.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for lang in "fr_FR" "sv_SE" "ru_RU" "de_DE" ; do 
+for lang in "fr_FR" "sv_SE" "ru_RU" "de_DE" "el_GR"  ; do 
     msgmerge -v ../po/${lang}/LC_MESSAGES/${1}.po  ../po/${2}.pot  > ../po/${lang}/LC_MESSAGES/${2}.po ; 
 done ;
