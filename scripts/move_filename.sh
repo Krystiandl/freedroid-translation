5ae73a49e106d185c498a04c9cdecd6c2f35048f
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-05-02 20:55:44 +0200

    add a line to the script to remove the old file

--- scripts/move_filename.sh
+++ scripts/move_filename.sh
@@ -2,4 +2,5 @@
 
 for lang in $(cat lang.txt)  ; do 
     msgmerge -v ../po/${lang}/LC_MESSAGES/${1}.po  ../po/${2}.pot  > ../po/${lang}/LC_MESSAGES/${2}.po ; 
+    rm -v ../po/${lang}/LC_MESSAGES/${1}.po
 done ;
