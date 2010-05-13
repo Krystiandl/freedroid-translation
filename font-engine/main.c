c95650f4eb719636f744be268d7b6a0e0a16b6db
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-05-13 12:25:27 +0200

    Update the file main.c :
    Add the headers stdio.h and stdlib.h, also SDL/SDL.h.
    main.c now return the ouptput code of TTF_Init().

--- font-engine/main.c
+++ font-engine/main.c
@@ -1,5 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
 
-int main(void) {
-    printf("hello world!\n") ;
+int main( int argc, char *argv[ ] )
+{
+
+    printf( "TTF_Init() : %d\n", TTF_Init()) ;
+    return EXIT_SUCCESS;
 }
