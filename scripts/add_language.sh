31387bdc05ccead451e33063f05760d42b212981
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-05-22 22:32:59 +0200

    correction to the script

--- scripts/add_language.sh
+++ scripts/add_language.sh
@@ -4,12 +4,15 @@
 cd ${HOME}/src/freedroid-translation/po/
 
 echo "for the language code ${1}"
-pwd
-if [ ! -d "${1}/LC_MESSAGES" ]; then 
+
+if [ ! -d "${1}" ]; then 
     mkdir "${1}" ;
-    mkdir "${i}/LC_MESSAGES" ;
 fi ;
 
+if [ ! -d "${1}/LC_MESSAGES" ] ; then
+    mkdir "${1}/LC_MESSAGES"
+fi ;
+        
 for i in *.pot ; do  # on all the pot files 
 
     msginit -i "${i}" -o "${1}/LC_MESSAGES/${i/.pot/.po}" -l ${1} -w 72 --no-translator ;
