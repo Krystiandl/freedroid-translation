38b5a403a15c5d878fb0c0e74caf80b8125d3baf
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-05-22 21:59:08 +0200

    remove relative pathname

--- scripts/upgrade_pot_files.sh
+++ scripts/upgrade_pot_files.sh
@@ -1,28 +1,17 @@
 #!/bin/sh
 # this script upgrade the pot files for the translatable files
 # listed below
-cd .. ;
-
-cd map ;
-git svn rebase ;
-cd ../src ;
-git svn rebase ;
-cd ../dialogs ;
-git svn rebase ; 
 
 # update the pot files for any msg string in the dialogs directory
-cd ..
-cd dialogs ;
+cd ${HOME}/src/freedroid-translation/dialogs ;
 for i in *.dialog ; do  # update the pot files
     ../scripts/extract-strings-from-data.py . "${i}" "../po/${i/.dialog/.pot}" ;
 done ;
 
 # update the pot files for any msg strings in the map directory
-cd .. ;
-cd map ;
+cd ${HOME}/src/freedroid-translation/map
 for i in $(grep -l '_"' *) ; do # update the pot files for items and title files
     ../scripts/extract-strings-from-data.py . "${i}" "../po/${i}.pot" ;
 done ;
 
-cd .. ;
 echo "update pot files done"
