afe9812b25dbb6ccc15448cbce07332505f8ed5b
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-06-01 13:22:48 +0200

    Move broken files with a .broken suffix so that they don't appear in transifex

--- scripts/upgrade_pot_files.sh
+++ scripts/upgrade_pot_files.sh
@@ -3,15 +3,32 @@
 # listed below
 
 # update the pot files for any msg string in the dialogs directory
+
+# go in the dialogs directory
 cd ${HOME}/src/freedroid-translation/dialogs ;
+
 for i in *.dialog ; do  # update the pot files
+    echo "extracting translatable strings in ${i}"
     ../scripts/extract-strings-from-data.py . "${i}" "../po/${i/.dialog/.pot}" ;
+    if !  $(msgfmt -c "../po/${i/.dialog/.pot}")  ; then
+        echo "../po/${i/.dialog/.pot} is broken"
+        mv -v "../po/${i/.dialog/.pot}" "../po/${i/.dialog/.pot.broken}"
+    fi
 done ;
 
 # update the pot files for any msg strings in the map directory
+
+# go in the map directory
 cd ${HOME}/src/freedroid-translation/map
+
+# 
 for i in $(grep -l '_"' *) ; do # update the pot files for items and title files
+    echo "extracting translatable strings in ${i}"
     ../scripts/extract-strings-from-data.py . "${i}" "../po/${i}.pot" ;
+    if ! $(msgfmt -c "../po/${i}.pot") ; then
+        echo "../po/${i}.pot is broken"
+        mv -v "../po/${i}.pot" "../po/${i}.pot..broken"
+    fi
 done ;
 
 echo "update pot files done"
