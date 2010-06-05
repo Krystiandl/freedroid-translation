3904fcebb4d708df047665666c5f23c8a6a6f974
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-06-05 12:58:13 +0200

    svn_rebase.sh update : Now the script is doing the commit of the synchronized source tree map, src and dialogs folders are commited to the main repo.

--- scripts/svn_rebase.sh
+++ scripts/svn_rebase.sh
@@ -7,4 +7,8 @@ git svn rebase
 cd ${HOME}/src/freedroid-translation/map
 git svn rebase
 cd ${HOME}/src/freedroid-translation/po
+
+cd ${HOME}/src/freedroid-translation
+git svn commit src map dialogs -m "synchronization with the main repository"
+
 git pull
