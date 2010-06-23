e7424e4271a458eeda14d35b8b7818585b2912c0
Elijah Balley <Ooh2iej7Ae@gmail.com>
2010-06-23 09:53:54 +0200

    Update of the move_filename.sh script$

--- scripts/move_filename.sh
+++ scripts/move_filename.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# first argument : old name of the dialog file
+# second argument : new name of the dialog file.
+
 for lang in $(cat lang.txt)  ; do 
     msgmerge -v ../po/${lang}/LC_MESSAGES/${1}.po  ../po/${2}.pot  > ../po/${lang}/LC_MESSAGES/${2}.po ; 
-    rm -v ../po/${lang}/LC_MESSAGES/${1}.po
+    git rm  ../po/${lang}/LC_MESSAGES/${1}.po
 done ;
+
+git rm ../po/${1}.pot
